0100ac4dc92497d62b40fb67de67f739d1c242fb
Jack Hsin <qk14000@gmail.com>
2022-03-09 17:38:16 +0800

    refactor: remove account controller

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -3,12 +3,10 @@ import { AccountService } from './account.service';
 import { AccountResolver } from './account.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountRepository } from './repositories/account.repository';
-import { AccountController } from './account.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AccountRepository])],
   providers: [AccountResolver, AccountService],
   exports: [AccountService],
-  controllers: [AccountController],
 })
 export class AccountModule {}
