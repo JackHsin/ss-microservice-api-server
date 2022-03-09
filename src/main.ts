75cac864c66af0212f122603b956c4f720a67586
Jack Hsin <qk14000@gmail.com>
2022-03-09 15:11:07 +0800

    feat: add service api port env

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const { SS_SERVICE_API_PORT } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(SS_SERVICE_API_PORT);
 }
 bootstrap();
